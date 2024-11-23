8e4f6cdbdb84d9b94d43d5dc1fc52444df32ccda
Sara Gajdovska <saragajdovska@Saras-MacBook-Air.local>
2024-11-23 18:17:35 +0100

    Implemented responsive navbar section

--- nextjs-lading-page/src/app/layout.tsx
+++ nextjs-lading-page/src/app/layout.tsx
@@ -11,7 +11,6 @@ const inter = Inter({
 
 export const metadata: Metadata = {
     title: "Modern Design Tool Landing Page",
-    description: "Created with the help of Frontend Tribe",
 };
 
 export default function RootLayout({
@@ -22,7 +21,7 @@ export default function RootLayout({
     return (
         <html lang="en">
             <body
-                className={`${inter.variable} font-sans antialiased bg-neutral-950 text-white`}
+                className={`${inter.variable} font-sans antialiased bg-neutral-950 text-white pb-[10000px]`}
             >
                 {children}
             </body>

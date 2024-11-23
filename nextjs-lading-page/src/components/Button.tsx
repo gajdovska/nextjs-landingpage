3fdaa102d21db0f32f790dff354cea67d86726b1
Sara Gajdovska <saragajdovska@Saras-MacBook-Air.local>
2024-11-24 00:41:37 +0100

    Implemented responsive Hero section

--- nextjs-lading-page/src/components/Button.tsx
+++ nextjs-lading-page/src/components/Button.tsx
@@ -1,11 +1,14 @@
-import React, { HTMLAttributes } from "react";
+import React, { ButtonHTMLAttributes, HTMLAttributes } from "react";
 import { cva } from "class-variance-authority";
 
-const classes = cva("border h-12 rounded-full px-6 font-bold", {
+const classes = cva("border h-12 rounded-full px-6 font-semibold", {
     variants: {
         variant: {
             primary: "bg-lime-400 text-neutral-950 border-lime-400",
             secondary: "border-white text-white bg-transparent",
+        },
+        size: {
+            sm: "h-10",
         },
     },
 });
@@ -13,13 +16,15 @@ const classes = cva("border h-12 rounded-full px-6 font-bold", {
 const Button = (
     props: {
         variant: "primary" | "secondary";
-    } & HTMLAttributes<HTMLButtonElement>
+        size?: "sm";
+    } & ButtonHTMLAttributes<HTMLButtonElement>
 ) => {
-    const { variant, className, ...otherProps } = props;
+    const { variant, className, size, ...otherProps } = props;
     return (
         <button
             className={classes({
                 variant: variant,
+                size: size,
                 className: className,
             })}
             {...otherProps}
--- nextjs-lading-page/src/sections/Hero.tsx
+++ nextjs-lading-page/src/sections/Hero.tsx
@@ -1,3 +1,53 @@
+import Button from "@/components/Button";
+import designExample1 from "@/assets/images/design-example-1.png";
+import designExample2 from "@/assets/images/design-example-2.png";
+import Image from "next/image";
+import Pointer from "@/components/Pointer";
+
 export default function Hero() {
-    return <div>Hero</div>;
+    return (
+        <section className="py-24 overflow-x-clip">
+            <div className="container relative">
+                <div className="lg:absolute -left-32 top-16 hidden lg:block">
+                    <Image src={designExample1} alt="Design Example 2 Image" />
+                </div>
+                <div className="lg:absolute -right-64 -top-16 hidden lg:block">
+                    <Image src={designExample2} alt="Design Example 1 Image" />
+                </div>
+                <div className="absolute left-56 top-96 hidden lg:block">
+                    <Pointer name="Andrea" />
+                </div>
+                <div className="absolute right-80 -top-5 hidden lg:block">
+                    <Pointer name="Bryan" color="red" />
+                </div>
+                <div className="flex justify-center">
+                    <div className="inline-flex py-1 px-3 bg-gradient-to-r from-purple-400 to-pink-400 rounded-full font-semibold text-neutral-950">
+                        ✨ $7.5M seed round raised
+                    </div>
+                </div>
+                <h1 className="text-6xl md:text-7xl lg:text-8xl mt-6 font-medium text text-center">
+                    Impactful design, created effortlessly
+                </h1>
+                <p className="text-center text-xl mt-8 text-white/50 max-w-2xl mx-auto">
+                    Design toold shouldn&apos;t slow you down. Layers combines
+                    powerful features with an intuitive itnerface that keeps you
+                    in your creative flow.
+                </p>
+                <form className="flex mx-auto border border-white/15 rounded-full p-2 mt-8 max-w-lg">
+                    <input
+                        type="email"
+                        placeholder="Enter your email"
+                        className="bg-transparent px-4 flex-1"
+                    />
+                    <Button
+                        type="submit"
+                        variant="primary"
+                        className="whitespace-nowrap"
+                    >
+                        Sign up
+                    </Button>
+                </form>
+            </div>
+        </section>
+    );
 }

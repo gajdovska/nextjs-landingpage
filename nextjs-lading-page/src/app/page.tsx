7531fa5785c4a8027d4638675b028cb8baba38bd
Sara Gajdovska <saragajdovska@Saras-MacBook-Air.local>
2024-11-24 01:20:45 +0100

    Implemented responsive Logo section

--- nextjs-lading-page/src/app/page.tsx
+++ nextjs-lading-page/src/app/page.tsx
@@ -1,11 +1,12 @@
 import Hero from "@/sections/Hero";
 import Navbar from "@/sections/Navbar";
-
+import LogoTicker from "@/sections/LogoTicker";
 export default function Home() {
     return (
         <>
             <Navbar />
             <Hero />
+            <LogoTicker />
         </>
     );
 }
